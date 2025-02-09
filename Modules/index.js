7137a7a38964be4716cbda84aa180719cb3d63bd
Duberticus <166752792+Duberticus@users.noreply.github.com>
2025-02-08 21:09:38 -0600

    modules

---
+++ Modules/index.js
@@ -0,0 +1,11 @@
+import {PI,getCircumference, getArea, getVolume } from './pi.js'
+
+console.log(PI)
+const circ = getCircumference(10);
+const vol =getVolume(10);
+const area = getArea(10);
+
+console.log(vol);
+console.log(area);
+
+
